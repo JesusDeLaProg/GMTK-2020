f8d1fc4af8b35c422618bcd93608c32a9dcbd497
sam <sambonnevile@gmail.com>
2020-07-10 22:25:03 -0400

    UI #1

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance {get;private set;}
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+        GameManager.instance = this;
+    }
+
+    public void StartLevel(){
+
+    }
+    
+    public void SetUI(){
+        
+    }
+
+    public void EndLevel(){
+
+    }
+
+    public void EndGame(){
+
+    }
+}
---
+++ Assets/Scripts/HealthDisplay.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthDisplay : MonoBehaviour
+{
+
+    private int _health = 5;
+    public Text Display;
+    public Image[] Healths;
+    public int health
+    {
+        get
+        {
+            return _health;
+        }
+        set
+        {
+            _health = value;
+        }
+    }
+
+    void Start()
+    {
+        Display.text = "Health : ";
+    }
+}
