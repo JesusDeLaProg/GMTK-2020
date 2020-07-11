0f98b1d539c8927b4c0b6b322478957569acc7ae
JesusDeLaProg <charland.maxime@gmail.com>
2020-07-10 21:59:33 -0400

    Simple PlayerController and Physics material

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 force = new Vector2(0, 0);
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            force += new Vector2(-1, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            force += new Vector2(1, 0);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            force += new Vector2(0, 1);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            force += new Vector2(0, -1);
+        }
+        rigidbody.AddForce(force);
+    }
+}
