ec0bc449fe60254a845efe8b252592c196d52bba
sam <sambonnevile@gmail.com>
2020-07-10 22:41:14 -0400

    Laser

---
+++ Assets/Scripts/LaserDisplay.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LaserDisplay : MonoBehaviour
+{
+    private bool _laser = true;
+    public Text Display;
+    public Image Lazer;
+    public bool laser
+    {
+        get
+        {
+            return _laser;
+        }
+        set
+        {
+            _laser = value;
+        }
+    }
+
+    void Start()
+    {
+        Display.text = "Laser : ";
+    }
+}
