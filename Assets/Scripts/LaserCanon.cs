19489052f885f9248b2de7faeb8fdd8b65ca68f9
JesusDeLaProg <charland.maxime@gmail.com>
2020-07-11 20:15:08 -0400

    Refactor Player Controller.

---
+++ Assets/Scripts/LaserCanon.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class LaserCanon : MonoBehaviour
+{
+    public float LaserLength = 100;
+    public float LaserWidth = 0.4f;
+    public float LaserLifeSpan = 0.5f;
+    public float LaserRecoil = 1;
+    public Material LaserMaterial;
+
+    private LineCreator lineCreator;
+    private new Rigidbody2D rigidbody;
+
+    public void Start()
+    {
+        lineCreator = GetComponent<LineCreator>();
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    public void Shoot()
+    {
+        var laser = lineCreator.DrawLineTo(transform.up, LaserMaterial, LaserLength, LaserWidth);
+        Destroy(laser, LaserLifeSpan);
+        rigidbody.AddForce(transform.up * -1 * LaserRecoil, ForceMode2D.Impulse);
+        DestroyAsteroidsInDirectionOf(transform.up);
+    }
+
+    private void DestroyAsteroidsInDirectionOf(Vector2 direction)
+    {
+        var thisPos = new Vector2(transform.position.x, transform.position.y);
+        var hits = Physics2D.RaycastAll(thisPos, direction, LaserLength);
+        foreach (var hit in hits.Where(h => h.transform.gameObject.CompareTag("Asteroid")))
+        {
+            var asteroid = hit.transform.gameObject.GetComponent<Asteroid>();
+            asteroid.Split(direction);
+        }
+    }
+}
---
+++ Assets/Scripts/LineCreator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineCreator : MonoBehaviour
+{
+    public LineRenderer DrawLineTo(Vector3 direction, Material material, float length, float width)
+    {
+        direction.Normalize();
+        var line = new GameObject();
+        var lineRenderer = line.AddComponent<LineRenderer>();
+        lineRenderer.material = material;
+        lineRenderer.numCapVertices = 40;
+        lineRenderer.startWidth = width;
+        lineRenderer.endWidth = width;
+        lineRenderer.SetPositions(new Vector3[] { transform.position, transform.position + direction * length });
+        return lineRenderer;
+    }
+}
